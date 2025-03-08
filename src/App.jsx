53d262a3ec037fe9e75901def28a5614482831f0
safin33221 <saifn33221@gmail.com>
2025-03-08 19:08:30 +0600

    initialize setup done

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+
+import Footer from './Components/Footer'
+import Navbar from './Components/Navbar'
+
+
+const App = () => {
+  return (
+    <div>
+      <nav>
+        <Navbar />
+      </nav>
+
+      <main>
+        {/* call your components inside main tag */}
+
+
+      </main>
+
+
+      <footer>
+        <Footer />
+      </footer>
+    </div>
+  );
+};
+
+export default App;
