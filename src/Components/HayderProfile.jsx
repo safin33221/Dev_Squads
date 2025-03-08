7b28f60166ddc13dc75ce273d9b138656610cc1b
hayder <hayderbd4290@gmail.com>
2025-03-08 20:28:25 +0600

    create profile image

--- src/Components/HayderProfile.jsx
+++ src/Components/HayderProfile.jsx
@@ -1,8 +1,19 @@
-
+import pic from '../assets/shortPic.jpg'
 
 const HayderProfile = () => {
   return (
-    <div>HayderProfile</div>
+    <div className='p-8'>
+        <div className="max-w-[1200px] flex flex-col md:flex-row py-12 px-6 mx-auto bg-slate-100">
+          {/* img */}
+          <div className="lg:w-1/2 flex justify-center items-center ">
+             <img src={pic} alt="" className="rounded-full w-[300px] h-[300px]" />
+          </div>
+          {/* info */}
+          <div className="lg:w-1/2">
+
+          </div>
+        </div>
+    </div>
   )
 }
 
